59c66a76c803ff0bf32d599b037a273a95b045b7
tabaarianna <74322375+tabaarianna@users.noreply.github.com>
2023-02-21 11:21:23 -0700

    Add files via upload

--- Scripts/PrioritySteering.cs
+++ Scripts/PrioritySteering.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrioritySteering : MonoBehaviour
+public class prioritySteering
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    float epsilon = .1f;
+    public BlendedSteering[] groups;
+
+    public SteeringOutput getSteering()
     {
-        
+        SteeringOutput steering = new SteeringOutput();
+        foreach (BlendedSteering g in groups)
+        {
+            steering = g.getSteering();
+
+            if (steering.linear.magnitude > epsilon || Mathf.Abs(steering.angular) > epsilon)
+            {
+                return steering;
+            }
+        }
+
+        return steering;
     }
 }
